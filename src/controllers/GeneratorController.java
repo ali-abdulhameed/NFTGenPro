0bcc1524c0835d07fa3e83950556b66a29ea950a
Ali Abdulhameed <66765610+ali-abdulhameed@users.noreply.github.com>
2022-05-31 01:43:47 -0500

    Update GeneratorController.java

--- src/controllers/GeneratorController.java
+++ src/controllers/GeneratorController.java
@@ -11,7 +11,6 @@ import java.util.LinkedList;
 import java.util.List;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.atomic.AtomicInteger;
-
 import javax.imageio.IIOImage;
 import javax.imageio.ImageIO;
 import javax.imageio.ImageWriteParam;
